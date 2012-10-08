355b7407d3106a99118e0a15bcc3a73e91bdfb76
michaelcockle <michael@axisweb.org>
2012-10-08 21:49:02 +0100

    ## process.php ##

--- process.php
+++ process.php
@@ -5,182 +5,97 @@ include_once "inc/header.html";
 ?>
 
 <div class="container">
-  
-  <div class="page-header"><h1><?php echo $page_title; ?> <small></small></h1></div>
 
-  <div class="span9">
-    <section>
+<div class="page-header"><h1><?php echo $page_title; ?> <small></small></h1></div>
 
-      <div class="row-fluid">
+<div class="span9">
+<section>
 
-        <div class="span9">
+<div class="row-fluid">
 
+<div class="span9">
 
-
-          
-
-
-          Version Control: Github
-          <a href="http://git-scm.com/documentation">Github Documentation</a>
-          Editor: Sublime Text 2
-          SASS / Compass (ruby / web matrix) 
-          Javascript: jQuery
-
-
-          <h3>Modernizr</h3>
-          <a href="http://modernizr.com/">Modernizr</a>
-
-<a href="http://yepnopejs.com/">YepNope.js for conditional loading of external .js and .css resources</a>
-          
-
-          <h3>OOCSS</h3>
-<ul>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-  <li></li>
-</ul>
 
 <ul>
-  <li><a href="https://github.com/stubbornella/oocss/wiki/FAQ">What is meant by an “object” in OOCSS?</a></li>
-  <li><a href="https://github.com/stubbornella/oocss/wiki">Object Oriented CSS</a></li>
-  <li><a href="http://www.stubbornella.org/content/2010/06/21/css-granularity-architecture/">How to create CSS objects? Get the granularity right!</a></li>
-  <li><a href="http://coding.smashingmagazine.com/2011/12/12/an-introduction-to-object-oriented-css-oocss/">An Introduction To Object Oriented CSS (OOCSS)</a></li>
-  <li><a href="http://www.bennadel.com/blog/1633-Object-Oriented-CSS-OOCSS-By-Nicole-Sullivan.htm">Object Oriented CSS (OOCSS) By Nicole Sullivan</a></li>
+    <li>Version Control: <a href="http://git-scm.com/documentation">Github</a></li>
+    <li>Editor: <a href="http://www.sublimetext.com/2">Sublime Text 2 </a></li> 
+    <!-- <li>SASS / Compass (ruby / Web Matrix)</li> -->
+    <li>Javascript: jQuery</li>
 </ul>
-          
-          
-          web ahead 
-          grids 
-
-<a href="http://5by5.tv/webahead/9">Mark Boulton on Grids - Podcast</a>
-
-          typography
-          
-          <a href="http://html5boilerplate.com/">HTML 5 Boilerplate</a>
 
 
+<h3>HTML / Files</h3>
 
-          Moodboards
-          Style Tiles ( OOCSS )
+<ul>
+    <li><a href="http://html5boilerplate.com/">HTML 5 Boilerplate</a></li>
+    <li><a href="http://modernizr.com/">Modernizr</a></li>
+    <li><a href="http://yepnopejs.com/">YepNope (conditional loading JS CSS files</a></li>
+</ul>
 
-          <a href="http://www.alistapart.com/articles/style-tiles-and-how-they-work/">Style Tiles and How They Work</a>
+<h3>CSS Framework</h3> 
+<!-- data URL's --> 
+<ul>
+    <li><a href="http://sass-lang.com/">SASS</a></li>
+    <li><a href="http://compass-style.org/">Compass</a> (auto sprite generator)</li>
+</ul>
 
 
-          Wireframes ( pen paper / illustrator? ) - Mobile first - 320px x 480px 
-          Prototyping (simple HTML CSS JS PHP includes + Apache)
+<h3>CSS</h3>
+<a href="http://selectivizr.com/">Selectivizr (CSS3 selectors for IE)</a>
+
+<h3>OOCSS</h3>
+<ul>
+    <li><a href="https://github.com/stubbornella/oocss/wiki/FAQ">What is meant by an “object” in OOCSS?</a></li>
+    <li><a href="https://github.com/stubbornella/oocss/wiki">Object Oriented CSS</a></li>
+    <li><a href="http://www.stubbornella.org/content/2010/06/21/css-granularity-architecture/">How to create CSS objects? Get the granularity right!</a></li>
+    <li><a href="http://coding.smashingmagazine.com/2011/12/12/an-introduction-to-object-oriented-css-oocss/">An Introduction To Object Oriented CSS (OOCSS)</a></li>
+    <li><a href="http://www.bennadel.com/blog/1633-Object-Oriented-CSS-OOCSS-By-Nicole-Sullivan.htm">Object Oriented CSS (OOCSS) By Nicole Sullivan</a></li>
+</ul>
 
 
-          Responsive web design
-          Mobile First (no @media query is the first media query)
-          
-          
-          <a href="http://selectivizr.com/">CSS3 selectors for IE</a>
-          
-          
+<h3>Grids </h3>
+<ul>
+<li><a href="http://5by5.tv/webahead/9">Mark Boulton on Grids - Podcast</a></li>
+</ul>     
+
+
 <h3>Browser Testing</h3>
-<a href="http://www.quirksmode.org/">QuirksMode.org - Browser Compatibility Tables</a>a>
-          <a href="http://www.quirksmode.org/compatibility.html">Compatibility Master Table</a>
+
+<ul>
+    <li><a href="http://www.quirksmode.org/">QuirksMode.org - Browser Compatibility Tables</a></li>
+    <li><a href="http://www.quirksmode.org/compatibility.html">Compatibility Master Table</a></li>
+</ul>
 
 
+<h3>Images <small>(Adaptive / Responsive Images)</small></h3>
+<ul>
+    <li><a href="http://www.slideshare.net/bryanrieger/rethinking-the-mobile-web-by-yiibu">Rethinking the mobile web</a></li>
+    <li><a href="http://adaptive-images.com/">Adaptive Images</a></li>
+    <li><a href="http://24ways.org/2011/adaptive-images-for-responsive-designs">Adaptive Images for Responsive Designs</a></li>
+    <li><a href="https://github.com/filamentgroup/Responsive-Images">filamentgroup / Responsive-Images</a></li>
+
+</ul>
 
 
-          <h3>Images</h3>
-          <ul>
-            <li><a href="http://www.slideshare.net/bryanrieger/rethinking-the-mobile-web-by-yiibu">Rethinking the mobile web</a></li>
-            <li><a href="http://adaptive-images.com/">Adaptive Images</a></li>
-            <li><a href="http://24ways.org/2011/adaptive-images-for-responsive-designs">Adaptive Images for Responsive Designs</a></li>
-            <li><a href="https://github.com/filamentgroup/Responsive-Images">filamentgroup / Responsive-Images</a></li>
-            <li></li>
-          </ul>
-
-
-          Content Out
+<h3>Content Out</h3>
+<p>Responsive web design / Mobile First (no @media query is the first media query)</p>
 
 <a href="http://www.netmagazine.com/interviews/mark-boulton-designing-websites-using-content-out">Mark Boulton on designing websites using 'content out</a>
 
-          Adaptive / Responsive Images
 
-          Optimisation, CSS sprites (compass auto sprite generator) , data URL's 
+<h3>Touch</h3>
 
+<ul>
+    <li><a href="http://jquerymobile.com/">jQuery Mobile</a></li>
+    <li><a href="https://github.com/senchalabs/jQTouch">senchalabs / jQTouch</a></li>
+    <li><a href="http://dev.sencha.com/deploy/touch/examples/production/kitchensink/#demo/touchevents">Sencha Touch Events</a></li>
+</ul>
 
-<a href="http://jquerymobile.com/">jQuery Mobile</a>
-<a href="https://github.com/senchalabs/jQTouch">senchalabs / jQTouch</a>
-<a href="http://dev.sencha.com/deploy/touch/examples/production/kitchensink/#demo/touchevents">Sencha Touch Events</a>
-        </div>
+</div>
+</div>
 
-
-      </div>
-
-
-
-
-
-      <h3>Xxxxx</h3>
-
-      <div class="row-fluid">
-        <ul class="thumbnails">
-          <li class="span3">
-            <a href="#" class="thumbnail">
-              <img src="/img/bs/260x180.gif" alt="">
-            </a>
-          </li>
-          <li class="span3">
-            <a href="#" class="thumbnail">
-              <img src="/img/bs/260x180.gif" alt="">
-            </a>
-          </li>
-          <li class="span3">
-            <a href="#" class="thumbnail">
-              <img src="/img/bs/260x180.gif" alt="">
-            </a>
-          </li>
-          <li class="span3">
-            <a href="#" class="thumbnail">
-              <img src="/img/bs/260x180.gif" alt="">
-            </a>
-          </li>
-        </ul>
-      </div>
-
-
-
-
-
-      <h3>Xxxxx</h3>
-
-      <ul class="thumbnails">
-        <li class="span4">
-          <a href="#" class="thumbnail">
-            <img src="/img/bs/360x270.gif" alt="">
-          </a>
-        </li>
-        <li class="span3">
-          <a href="#" class="thumbnail">
-            <img src="/img/bs/260x120.gif" alt="">
-          </a>
-        </li>
-        <li class="span2">
-          <a href="#" class="thumbnail">
-            <img src="/img/bs/160x120.gif" alt="">
-          </a>
-        </li>
-        <li class="span3">
-          <a href="#" class="thumbnail">
-            <img src="/img/bs/260x120.gif" alt="">
-          </a>
-        </li>
-        <li class="span2">
-          <a href="#" class="thumbnail">
-            <img src="/img/bs/160x120.gif" alt="">
-          </a>
-        </li>
-      </ul>
-    </section>
-  </div><!-- end span9 -->
-
-
+</section>
+</div><!-- end span9 -->
 
 </div><!-- end container -->
 
